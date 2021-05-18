67ef61e065bc73bb5756b3976fce5c85059681e8
Pasquale <pasquale.basile@infocube.it>
2021-05-18 19:28:08 +0200

    Commit GoogleSearch Module

--- src/test/java/com/example/selenium_demo/GooglePageTest.java
+++ src/test/java/com/example/selenium_demo/GooglePageTest.java
@@ -1,20 +1,12 @@
 package com.example.selenium_demo;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverInfo;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
 
 public class GooglePageTest {
 
